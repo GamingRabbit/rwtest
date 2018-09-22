48cc370864d8e90e145695c730b0a649e9fa4854
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-09-22 12:02:11 +0200

    Update rwtesttest.py

--- rwtesttest.py
+++ rwtesttest.py
@@ -20,5 +20,13 @@ async def say(ctx, message):
 async def hey(ctx):
     responses =["That is a resounding no", "It is not looking likely", "Too hard to tell", "It is quite possible", "Definitely", "no, sir", "yes", "of course!"]
     await ctx.send(random.choice(responses))
-
+@client.command()
+async def counter(ctx):
+    embed = discord.Embed(title="Counter", description="Server info", color=0x6FA8DC)
+    embed.add_field(name="Name", value=ctx.message.guild.name)
+    embed.add_field(name="Owner", value=ctx.message.guild.owner.mention)
+    embed.add_field(name="Members", value=ctx.message.guild.member_count)
+    embed.set_thumbnail(url=ctx.message.guild.icon_url)
+    await ctx.send(embed=embed)
+    
 client.run(os.getenv("TOKEN"))
